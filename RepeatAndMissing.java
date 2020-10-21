013626d6055012097ea3520cbe98b17d1fc55b48
Harsh <thehjain@gmail.com>
2020-10-21 18:34:52 +0530

    Repeat and Missing Number

--- RepeatAndMissing.java
+++ RepeatAndMissing.java
@@ -1,24 +1,47 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+//We have given an array of N size
+//Array is consisiting of number from 1 to N
+//One is number is missing and one is repeating
+//we have to find both
 
-class CodeChef {
+//First thing is to find it after sorting the array
+//it will take O(nlogn)
 
-	private static int MAX = Integer.MAX_VALUE;
-	private static int MIN = Integer.MIN_VALUE;
+//Second thing is to use another array or hashet
+//We can do that in O(n)
 
-	public static void main(String[] args) throws IOException {
+//But we can do this question in O(N) without using extra space
+//Using Xor
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+void getTwoElements(int[] arr, int n) {
 
-		int T = Integer.parseInt(br.readLine());
+	int x = 0, y = 0;
 
-		while (T-- != 0) {
+	int xor = arr[0];
+	int set_bit;
 
+	for (int i = 1; i < n; i++)
+		xor ^= arr[i];
 
+	for (int i = 1; i <= n; i++)
+		xor ^= i;
 
-		}
+	set_bit = xor & ~(xor - 1);
 
+	for (int i = 0; i < n; i++) {
+		if ((arr[i] & set_bit) != 0)
+			x ^= arr[i];
+		else
+			y ^= arr[i];
 	}
 
+	for (int i = 1; i <= n; i++) {
+		if ((i & set_bit) != 0)
+			x ^= i;
+		else
+			y ^= i;
+	}
+
+	//And here we got our x and y
+
 }
+
