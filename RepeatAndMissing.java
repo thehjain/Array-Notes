caa11f36fe2eace8a7071334a65bd8566de2ae6e
Harsh <thehjain@gmail.com>
2020-10-21 11:41:57 +0530

    Repeat and Missing

---
+++ RepeatAndMissing.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class CodeChef {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		while (T-- != 0) {
+
+
+
+		}
+
+	}
+
+}
